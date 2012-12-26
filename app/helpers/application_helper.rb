d21e65a9fa9fbf1a37200719074ee681989c1fd4
haidao <haidao@server>
2012-12-26 16:42:04 +0800

    title-to-chinese:with helper function #50

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # return a full title
+  def full_title(page_title)  
+    base_title = "#{t('layout.19wu')} - #{t('slogan.title')}"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end  
 end
